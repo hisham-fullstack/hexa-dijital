9cb9dae0e651d1e2bd71ab848cfc3f8e9df9e2cc
Hisham <zhisolk@gmail.com>
2026-09-07 19:14:21 +0300

    meta

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -75,6 +75,7 @@ export default function RootLayout({ children }) {
           className={`${plusJakarta.className} ${plusJakarta.variable} ${inter.variable}`}
           suppressHydrationWarning
         >
+          {/* Google Analytics */}
           <Script
             src="https://www.googletagmanager.com/gtag/js?id=G-2C1CWMLDYY"
             strategy="afterInteractive"
@@ -87,6 +88,31 @@ export default function RootLayout({ children }) {
               gtag('config', 'G-2C1CWMLDYY');
             `}
           </Script>
+
+          {/* Meta (Facebook) Pixel */}
+          <Script id="meta-pixel" strategy="afterInteractive">
+            {`
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1082357000959860');
+              fbq('track', 'PageView');
+            `}
+          </Script>
+          <noscript>
+            <img
+              height="1"
+              width="1"
+              style={{ display: "none" }}
+              src="https://www.facebook.com/tr?id=1082357000959860&ev=PageView&noscript=1"
+              alt=""
+            />
+          </noscript>
 
           <Providers>
             <Preloader />
